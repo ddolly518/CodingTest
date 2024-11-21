1a9536094475214c5a61bc3167402f8330e1f9ca
KMS <80440158+ddolly518@users.noreply.github.com>
2024-11-21 23:33:58 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 184 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int A1 = in.nextInt();
+        int A2 = in.nextInt();
+        int A3 = in.nextInt();
+        int A4 = in.nextInt();
+        int A5 = in.nextInt();
+        int A6 = in.nextInt();
+        
+        System.out.print(1-A1+" ");
+        System.out.print(1-A2+" ");
+        System.out.print(2-A3+" ");
+        System.out.print(2-A4+" ");
+        System.out.print(2-A5+" ");
+        System.out.print(8-A6);
+    }
+}
