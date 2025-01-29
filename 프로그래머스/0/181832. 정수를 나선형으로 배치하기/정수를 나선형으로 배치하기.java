cc39045f4a9fa05e98d97286aa3d67c679d7b3b5
KMS <80440158+ddolly518@users.noreply.github.com>
2025-01-29 20:30:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.04 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -5,24 +5,21 @@ class Solution {
         int start=0;
         int end=n-1;
         while (num <= n*n) {
-            // 왼쪽에서 오른쪽으로
-            for (int i = start; i < end; i++) {
+             // 왼쪽에서 오른쪽으로
+            for (int i = start; i <= end; i++) {
                 answer[start][i] = num++;
             }
             // 위에서 아래로
-            for (int i = start; i < end; i++) {
+            for (int i = start + 1; i <= end; i++) {
                 answer[i][end] = num++;
             }
             // 오른쪽에서 왼쪽으로
-            for (int i = end; i > start; i--) {
+            for (int i = end - 1; i >= start; i--) {
                 answer[end][i] = num++;
             }
             // 아래에서 위로
-            for (int i = end; i > start; i--) {
+            for (int i = end - 1; i > start; i--) {
                 answer[i][start] = num++;
-            }
-            if (start == end) {
-                answer[start][start] = num++;
             }
             start++;
             end--;
