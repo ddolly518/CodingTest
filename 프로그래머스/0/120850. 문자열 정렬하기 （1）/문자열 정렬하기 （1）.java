05de703a34764b540f2f3f4737601c4432fbaa05
KMS <80440158+ddolly518@users.noreply.github.com>
2025-02-09 21:24:45 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.54 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        List<Integer> list = new ArrayList<>();
+        for (char ch : my_string.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                list.add(ch-'0');
+            }
+        }
+        int[] answer = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
