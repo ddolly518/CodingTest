d5a4eca576ccdce248bc59fece768e1f89236bb2
KMS <80440158+ddolly518@users.noreply.github.com>
2025-02-17 16:54:37 +0900

    [level 0] Title: 숫자 찾기, Time: 9.93 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String str = num+"";
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i)==(k+'0')) 
+                return i+1;
+        }
+        return answer;
+    }
+}
