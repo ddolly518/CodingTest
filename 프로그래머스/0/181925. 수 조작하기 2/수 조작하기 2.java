d0e0fef9f94cbe80593fb6d77b6796b9d50942bf
KMS <80440158+ddolly518@users.noreply.github.com>
2025-03-12 21:16:58 +0900

    [level 0] Title: 수 조작하기 2, Time: 1177.36 ms, Memory: 384 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -4,12 +4,11 @@ class Solution {
         for (int i=0; i<numLog.length-1; i++) {
             int num = numLog[i+1]-numLog[i];
             switch (num) {
-                case 1 : answer+='w'; break;
-                case -1 : answer+='s'; break;
-                case 10 : answer+='d'; break;
-                case -10 : answer+='a'; break;
-                    default : break;
-            }
+                case 1 -> answer+='w';
+                case -1 -> answer+='s';
+                case 10 -> answer+='d';
+                case -10 -> answer+='a';
+            }    
         }
         return answer;
     }
