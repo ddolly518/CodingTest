38209b351dbf965b4c49810032be8130752cdb67
KMS <80440158+ddolly518@users.noreply.github.com>
2025-03-28 15:23:20 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        int[] arr = new int[201];
+        int[] arr = new int[200];
         for (int[] line:lines) {
             int start = line[0];
             int end = line[1];
