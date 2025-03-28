22fac958f0117cc0b251591fd06e83592b7edcb4
KMS <80440158+ddolly518@users.noreply.github.com>
2025-03-28 15:17:29 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(String A, String B) {
         int answer = 0;
-        if (A.equals(B))
-            return 0;
-        A=A.repeat(2);
-        for (int i=0; i<A.length()/2; i++) {
-            if (B.equals(A.substring(A.length()/2-i,A.length()-i)))
-                return answer;
-            answer++;
-        }
-        return -1;
+        
+        
+        B=B.repeat(2);
+        return B.indexOf(A);
+        //return -1;
     }
 }
