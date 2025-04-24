ad308bc9d392052985d30bfee1c5d74b64ce97ed
KMS <80440158+ddolly518@users.noreply.github.com>
2025-04-24 15:17:04 +0900

    [level 1] Title: 실패율, Time: 59.82 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int num = stages.length;
+        double[][] rate = new double[N][2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int stage : stages) {
+            map.put(stage, map.getOrDefault(stage,0)+1);
+        }
+        for (int i=0; i<N; i++) {
+            rate[i][0]=i;
+            rate[i][1]=num==0?0:(double)map.getOrDefault(i+1, 0)/num;
+            num-=map.getOrDefault(i+1, 0);
+        }
+        Arrays.sort(rate,(a,b) -> {
+            if (Double.compare(b[1], a[1]) != 0)
+                return Double.compare(b[1],a[1]);
+            return Double.compare(a[0],b[0]);
+        });
+        for (int i=0; i<N; i++) {
+            answer[i]=(int)rate[i][0]+1;
+        }
+        return answer;
+    }
+}
