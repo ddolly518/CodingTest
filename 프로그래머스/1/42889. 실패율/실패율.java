b5db954afdf22ea36a31cc3b6aa5d9ce490ba151
KMS <80440158+ddolly518@users.noreply.github.com>
2025-04-24 15:34:48 +0900

    [level 1] Title: 실패율, Time: 38.13 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -15,7 +15,7 @@ class Solution {
             num-=map.getOrDefault(i+1, 0);
         }
         Arrays.sort(rate,(a,b) -> {
-            if (Double.compare(b[1], a[1]) != 0)
+            if (Double.compare(a[1], b[1]) != 0)
                 return Double.compare(b[1],a[1]);
             return Double.compare(a[0],b[0]);
         });
