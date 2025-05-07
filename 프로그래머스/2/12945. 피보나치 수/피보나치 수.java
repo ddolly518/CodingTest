42e93628d86cb88e3038a96f8176dd9c94088c67
KMS <80440158+ddolly518@users.noreply.github.com>
2025-05-07 20:58:26 +0900

    [level 2] Title: 피보나치 수, Time: 2.77 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = (a + b) % 1234567;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
