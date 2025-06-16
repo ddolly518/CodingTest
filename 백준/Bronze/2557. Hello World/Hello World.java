8faaeaa004e30f13159ed78f5391bc563248eb04
KMS <80440158+ddolly518@users.noreply.github.com>
2025-06-16 14:16:17 +0900

    [Bronze V] Title: Hello World, Time: 108 ms, Memory: 13852 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,7 +1,5 @@
-import java.util.*;
-
-class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
     }
 }
