441d287826677d559599f2378d6e3d6d2d1d0af5
KMS <80440158+ddolly518@users.noreply.github.com>
2025-06-16 14:24:48 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Integer A = in.nextInt();
-        Integer B = in.nextInt();
-        System.out.print(A+B);
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
     }
 }
