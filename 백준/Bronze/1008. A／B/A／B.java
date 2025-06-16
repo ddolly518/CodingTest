56bfe51fa66ff94ba595bd6f576f22e6a90aeecf
KMS <80440158+ddolly518@users.noreply.github.com>
2025-06-16 14:51:48 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 18016 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        System.out.print((double)A/B);
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        int B = sc.nextInt();
+        System.out.println(A/B);
     }
 }
