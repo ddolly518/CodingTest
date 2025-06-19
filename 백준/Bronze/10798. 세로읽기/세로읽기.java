11247bdf7cb11b744874d0bcce0ae403a45fbd35
KMS <80440158+ddolly518@users.noreply.github.com>
2025-06-19 17:44:28 +0900

    [Bronze I] Title: 세로읽기, Time: 136 ms, Memory: 16300 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = new String[5];
+        String answer = "";
+        int len = 0;
+        for (int i=0; i<5; i++) {
+            arr[i] = br.readLine();
+            len = Math.max(len, arr[i].length());
+        }
+        for (int i=0; i<len; i++) {
+            for (int j=0; j<5; j++) {
+                if (arr[j].length()>i) {
+                    answer+=arr[j].charAt(i);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
