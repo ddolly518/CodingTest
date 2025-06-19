1ede0a8804ae79f978186d8af2ff1da059a06627
KMS <80440158+ddolly518@users.noreply.github.com>
2025-06-19 17:49:07 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 16304 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -2,22 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = new String[5];
-        String answer = "";
-        int len = 0;
-        for (int i=0; i<5; i++) {
-            arr[i] = br.readLine();
-            len = Math.max(len, arr[i].length());
-        }
-        for (int i=0; i<len; i++) {
-            for (int j=0; j<5; j++) {
-                if (arr[j].length()>i) {
-                    answer+=arr[j].charAt(i);
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] arr = new String[5];
+            String answer = "";
+            int len = 0;
+            for (int i=0; i<5; i++) {
+                arr[i] = br.readLine();
+                len = Math.max(len, arr[i].length());
+            }
+            for (int i=0; i<len; i++) {
+                for (int j=0; j<5; j++) {
+                    if (arr[j].length()>i) {
+                        answer+=arr[j].charAt(i);
+                    }
                 }
             }
+            System.out.println(answer);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(answer);
     }
 }
