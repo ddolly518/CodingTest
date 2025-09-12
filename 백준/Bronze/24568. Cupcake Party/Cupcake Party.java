ab8f7f57bc2fcda9004096d979986f55949c0103
KMS <80440158+ddolly518@users.noreply.github.com>
2025-09-12 20:19:14 +0900

    [Bronze V] Title: Cupcake Party, Time: 176 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int m = sc.nextInt();
+        int num = n*8+m*3-28;
+        System.out.println(num);
+    }
+}
