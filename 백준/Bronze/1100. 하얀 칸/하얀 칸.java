4d8e10a6067b7325b63ac8c2c8c26ecd6f7b7a50
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-10 21:57:05 +0900

    [Bronze II] Title: 하얀 칸, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for (int i=0; i<8; i++) {
+            String str = br.readLine();
+            if (i%2==0) {
+                for (int j=0; j<8; j+=2) {
+                    char ch = str.charAt(j);
+                    if (ch=='F')
+                        count++;
+                }
+            } else {
+                for (int j=1; j<8; j+=2) {
+                    char ch = str.charAt(j);
+                    if (ch=='F')
+                        count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
