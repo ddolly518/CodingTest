7fdd3615a952b0fcfc2d29b42b01dabd3ab68fb2
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-11 16:57:22 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        int answer = -1;
+        
         if (num%4==0 && (num%100!=0 || num%400==0)) {
-            System.out.println("1");
+            answer = 1;
         } else {
-            System.out.println("0");
+            answer = 0;
         }
+        
+        System.out.print(answer);
     }
 }
