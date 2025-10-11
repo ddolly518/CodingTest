90b9e953e1fa0be27d5c1632134c99032442ada4
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-11 17:16:53 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,29 +1,31 @@
-import java.util.*; 
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        int answer = 0;
+        
         for (int i=0; i<3; i++) {
-            int n = sc.nextInt();
-            map.put(n,map.getOrDefault(n,0)+1);
+            int num = sc.nextInt();
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        if (map.size()==1) {
-            for (int key : map.keySet()) {
-                System.out.println(10000+key*1000);
-            }
-        } else if (map.size()==2) {
-            for (int key : map.keySet()) {
-                if (map.get(key)==2) {
-                    System.out.println(1000+key*100);
-                }
-            }
-        } else {
+        
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
             int max = 0;
-            for (int key : map.keySet()) {
-                max = Math.max(key,max);
+            if (entry.getValue()==3) {
+                int num = 10000+entry.getKey()*1000;
+                answer = Math.max(answer, num);
+            } else if (entry.getValue()==2) {
+                int num = 1000+entry.getKey()*100;
+                answer = Math.max(answer, num);
+            } else {
+                max = Math.max(max, entry.getKey());
+                int num = 100*max;
+                answer = Math.max(answer, num);
             }
-            System.out.println(max*100);
         }
+        
+        System.out.print(answer);
     }
 }
