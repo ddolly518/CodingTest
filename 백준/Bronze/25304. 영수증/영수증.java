6e79d99b10f9a18ec533feb7e8cb90e1fd54650e
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-12 20:09:37 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,15 +7,14 @@ public class Main {
         int X = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
         for (int i=0; i<N; i++) {
-            StringTokenizer str = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             X-=(a*b);
         }
-        if (X==0) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        if (X==0)
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
