ce0f0e21060e35fad151c6c54f2d1e76867254dd
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-12 21:04:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 184 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for(int i=1; i<=N; i++) {
-           for (int j=0; j<i; j++) {
-               System.out.print("*");
-           } 
-           System.out.println();
+        String str = "*";
+        
+        for (int i=1; i<=N; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j=1; j<=i; j++) {
+                sb.append(str);
+            }
+            System.out.println(sb.toString());
         }
     }
 }
