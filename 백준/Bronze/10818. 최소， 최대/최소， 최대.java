4132b26ec41c240ce988e1db6a596506dee9b55d
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-13 18:56:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 88328 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,15 +4,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
         for (int i=0; i<N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            min = Math.min(num,min);
-            max = Math.max(num,max);
+            max = Math.max(max,num);
+            min = Math.min(min,num);
         }
-        System.out.println(min+" "+max);
+        bw.write(min+" "+max);
+        bw.flush();
     }
 }
