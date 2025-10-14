abcc59e75a8b5d603fa498131a6a1017297ba7ca
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-14 23:44:26 +0900

    [Bronze II] Title: 상수, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] arr = str.split(" ");
-        StringBuilder sb = new StringBuilder(arr[0]);
-        arr[0] = sb.reverse().toString();
-        sb = new StringBuilder(arr[1]);
-        arr[1] = sb.reverse().toString();
-        System.out.println(Math.max(Integer.parseInt(arr[0]), Integer.parseInt(arr[1])));
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int one = (A%10)*100 + (A%100/10)*10 + (A/100);
+        int two = (B%10)*100 + (B%100/10)*10 + (B/100);
+        int num = Math.max(one, two);
+        System.out.print(num);
     }
 }
