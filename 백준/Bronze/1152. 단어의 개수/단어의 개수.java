926d13f6471ccf3623abcc5aa0ce2ed40d40de59
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-14 23:37:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 484 ms, Memory: 63504 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().trim();
         if (str.isEmpty()) {
-            System.out.println(0);
+            System.out.print(0);
         } else {
             String[] arr = str.split(" ");
-            System.out.println(arr.length);   
-        }        
-    } 
+            System.out.print(arr.length);
+        }
+    }
 }
