d9af2b7c64b9769bf6b2955a112f2af0bf12f568
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-14 13:33:14 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -4,14 +4,14 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[30];
-        for (int i=0; i<28; i++) {
-            int n = Integer.parseInt(br.readLine())-1;
-            arr[n] = 1;
+        int[] arr = new int[31];
+        for (int i=1; i<=28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[num] = 1;
         }
-        for (int i=0; i<30; i++) {
+        for (int i=1; i<=30; i++) {
             if (arr[i]==0) {
-                System.out.println(i+1);
+                System.out.println(i);
             }
         }
     }
