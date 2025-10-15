7fb15dad15c6eb3ea8b18b76e93c475c8e4af2bf
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-15 14:49:06 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 192 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -4,15 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for (int i=0; i<2*N-1; i++) {
-            int num = Math.abs(i-(N-1));
-            for (int j=0; j<num; j++) {
-                System.out.print(" ");
+    
+        for (int i=1; i<2*N; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i<=N) {
+                for (int j=0; j<(N-i); j++) {
+                    sb.append(" ");
+                }
+                for (int j=1; j<=(2*i-1); j++) {
+                    sb.append("*");
+                }
+            } else {
+                for (int j=0; j<(i-N); j++) {
+                    sb.append(" ");
+                }
+                for (int j=1; j<=(2*(2*N-i)-1); j++) {
+                    sb.append("*");
+                }
             }
-            for (int j=0; j<2*N-1-2*num; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println(sb.toString());
         }
     }
 }
