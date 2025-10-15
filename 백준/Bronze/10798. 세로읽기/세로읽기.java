44593e4757934ad30957b5e7155191e5db7d7803
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-15 17:17:16 +0900

    [Bronze I] Title: 세로읽기, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -2,26 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String[] arr = new String[5];
-            String answer = "";
-            int len = 0;
-            for (int i=0; i<5; i++) {
-                arr[i] = br.readLine();
-                len = Math.max(len, arr[i].length());
-            }
-            for (int i=0; i<len; i++) {
-                for (int j=0; j<5; j++) {
-                    if (arr[j].length()>i) {
-                        answer+=arr[j].charAt(i);
-                    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        String line;
+        int max = 0;
+        
+        while ((line=br.readLine())!=null) {
+            list.add(line);
+            max = Math.max(max,line.length());
+        }
+        for (int i=0; i<max; i++) {
+            for (int j=0; j<list.size(); j++) {
+                String str = list.get(j);
+                if (str.length()>i) {
+                    sb.append(str.charAt(i));
                 }
             }
-            System.out.println(answer);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        System.out.print(sb.toString());
     }
 }
