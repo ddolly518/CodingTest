1c4246de48181cfce54380d810cf7528aa9345cb
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-15 15:20:19 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 116 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -6,28 +6,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int answer = 0;
+        
         for (int i=0; i<N; i++) {
-            String str = br.readLine();
             Stack<Character> stack = new Stack<>();
+            String str = br.readLine();
             boolean bo = true;
             for (int j=0; j<str.length(); j++) {
                 char ch = str.charAt(j);
-                if (stack.isEmpty()) {
-                    stack.push(ch);
+                if (!stack.isEmpty() && stack.peek()!=ch && stack.contains(ch)) {
+                    bo = false;
+                    break;
                 } else {
-                    if (stack.peek()!=ch) {
-                        if (!stack.contains(ch)) {
-                            stack.push(ch);
-                        } else {
-                            bo = false;
-                        }
-                    }
+                    stack.push(ch);
                 }
             }
             if (bo) {
                 answer++;
             }
         }
-        System.out.println(answer);
+        
+        System.out.print(answer);
     }
 }
