f51d3f598bf2972b4a445948ce87c477cb35aba2
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-15 14:31:27 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 184 ms, Memory: 18340 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] arr = str.split(" ");
-        int[] num = {1,1,2,2,2,8};
-        for (int i=0; i<6; i++) {
-            int n = Integer.parseInt(arr[i]);
-            System.out.print(num[i]-n);
-            System.out.print(" ");
+        String line = sc.nextLine();
+        String[] arr = line.split(" ");
+        int[] count = new int[]{1,1,2,2,2,8};
+        
+        for (int i=0; i<count.length; i++) {
+            int num = Integer.parseInt(arr[i]);
+            System.out.print(count[i]-num+" ");
         }
     }
 }
