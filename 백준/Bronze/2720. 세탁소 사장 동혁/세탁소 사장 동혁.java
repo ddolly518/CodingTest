9e43a0a08487ff78109fe67f2b7bd34196f183fc
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-16 13:54:33 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 156 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -5,10 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         for (int i=0; i<T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.print(n/25+" "+n%25/10+" "+n%25%10/5+" "+n%25%10%5);
-            System.out.println();
+            int num = Integer.parseInt(br.readLine());
+            int q = num/25;
+            int d = num%25/10;
+            int n = num%25%10/5;
+            int p = num%25%10%5;
+            bw.write(q+" "+d+" "+n+" "+p+"\n");
         }
+        bw.flush();
     }
 }
