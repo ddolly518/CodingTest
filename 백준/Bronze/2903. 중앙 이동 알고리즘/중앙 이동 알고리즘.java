32df2ed5c830f8f4c9fccf5c89b71c40fa8d12fa
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-16 15:17:29 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
+    
+    static int answer = 2;
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println((int)Math.pow((Math.pow(2,n)+1),2));
+        for (int i=0; i<n; i++) {
+            answer = 2*answer-1;
+        }
+        System.out.print(answer*answer);
     }
 }
