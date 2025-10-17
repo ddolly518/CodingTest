1edfff9b6a6f5e99269985884c207140c24e5699
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-17 12:29:44 +0900

    [Bronze III] Title: 배수와 약수, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -4,21 +4,25 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         while (true) {
+            String answer;
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if ((a+b)==0)
+            if (a==0 && b==0) {
                 break;
-            String answer = "";
-            if (b%a==0) {
-                answer = "factor";
-            } else if (a%b==0) {
-                answer = "multiple";
-            } else {
-                answer = "neither";
             }
-            System.out.println(answer);
+            if (b%a==0) {
+                answer="factor";
+            } else if (a%b==0) {
+                answer="multiple";
+            } else {
+                answer="neither";
+            }
+            bw.write(answer+"\n");
         }
+        bw.flush();
     }
 }
