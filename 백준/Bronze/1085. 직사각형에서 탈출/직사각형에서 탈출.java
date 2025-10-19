77f7ee202e10ff427735a62f95a4ecb58de4cc86
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-19 15:00:48 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 168 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,16 +1,18 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-        int h = Integer.parseInt(st.nextToken());
-        int a = Math.min(x,w-x);
-        int b = Math.min(y,h-y);
-        System.out.println(Math.min(a,b));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        
+        min = Math.min(min,x);
+        min = Math.min(min,y);
+        min = Math.min(min,w-x);
+        min = Math.min(min,h-y);
+        System.out.print(min);
     }
 }
