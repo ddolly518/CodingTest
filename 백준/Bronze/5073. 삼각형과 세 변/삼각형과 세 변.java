a2580b8051b9e5e0326145ef11120086121e04f9
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-19 16:21:57 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -4,32 +4,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[3];
-        for (int i=0; i<3; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        while ((arr[0]+arr[1]+arr[2])!=0) {
-            Arrays.sort(arr);
-            if (arr[0]+arr[1]<=arr[2])
-                System.out.println("Invalid");
-            else {
-                Map<Integer, Integer> map = new HashMap<>();
-                for (int i=0; i<3; i++) {
-                    map.put(arr[i], map.getOrDefault(arr[i],0)+1);   
-                }
-                if (map.size()==1)
-                    System.out.println("Equilateral");
-                else if (map.size()==2)
-                    System.out.println("Isosceles");
-                else 
-                    System.out.println("Scalene");
-            }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int one = Integer.parseInt(st.nextToken());
+            int two = Integer.parseInt(st.nextToken());
+            int three = Integer.parseInt(st.nextToken());
             
-            st = new StringTokenizer(br.readLine());
-            for (int i=0; i<3; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
+            if (one==0 && two==0 && three==0)
+                break;
+
+            String answer = fun(one,two,three);
+            bw.write(answer+"\n");
         }
+        bw.flush();
+    }
+    
+    private static String fun(int one, int two, int three) {
+        int sum = one+two+three;
+        int max = Math.max(one, two);
+        max = Math.max(max, three);
+        if (sum<=2*max)
+            return "Invalid";
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(one,map.getOrDefault(one,0)+1);
+        map.put(two,map.getOrDefault(two,0)+1);
+        map.put(three,map.getOrDefault(three,0)+1);
+        
+        if (map.size()==1)
+            return "Equilateral";
+        else if (map.size()==2)
+            return "Isosceles";
+        else
+            return "Scalene";
     }
 }
