e6c3c48f7d6b5b6d960b6000ba9edb012e2b70e5
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-19 15:55:13 +0900

    [Bronze III] Title: 대지, Time: 256 ms, Memory: 27232 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -6,18 +6,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int min_x = Integer.MAX_VALUE;
-        int max_x = Integer.MIN_VALUE;
         int min_y = Integer.MAX_VALUE;
+        int max_x = Integer.MIN_VALUE;
         int max_y = Integer.MIN_VALUE;
+        
         for (int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            min_x = Math.min(a,min_x);
-            max_x = Math.max(a,max_x);
-            min_y = Math.min(b,min_y);
-            max_y = Math.max(b,max_y);
+            int one = Integer.parseInt(st.nextToken());
+            min_x = Math.min(min_x,one);
+            max_x = Math.max(max_x,one);
+            int two = Integer.parseInt(st.nextToken());
+            min_y = Math.min(min_y,two);
+            max_y = Math.max(max_y,two);
         }
-        System.out.println((max_x-min_x)*(max_y-min_y));
+        int answer = (max_x-min_x)*(max_y-min_y);
+        System.out.print(answer);
     }
 }
