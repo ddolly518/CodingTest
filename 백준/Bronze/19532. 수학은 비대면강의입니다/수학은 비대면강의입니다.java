58455edffaaf21264f8d237df28f52c29d64a509
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-20 15:33:45 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 184 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,18 +1,18 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        double a = Double.parseDouble(st.nextToken());
-        double b = Double.parseDouble(st.nextToken());
-        double c = Double.parseDouble(st.nextToken());
-        double d = Double.parseDouble(st.nextToken());
-        double e = Double.parseDouble(st.nextToken());
-        double f = Double.parseDouble(st.nextToken());
-        int x = (int)((c*e - b*f) / (a*e - b*d));
-        int y = (int)((a*f - c*d) / (a*e - b*d));
-        System.out.println(x+" "+y);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        
+        int x = (c*e-b*f)/(a*e-b*d);
+        int y = (c*d-a*f)/(b*d-a*e);
+        
+        System.out.print(x+" "+y);
     }
 }
