e15a22c5247a1036c39879798d1cc59dcccde614
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-20 15:01:15 +0900

    [Bronze II] Title: 분해합, Time: 1388 ms, Memory: 310000 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,24 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int len = String.valueOf(n).length();
-        int start = Math.max(1, n - len * 9);
-        int answer = 0;
-        for (int i=start; i<n; i++) {
-            int num = i;
-            int count = i;
-            while (num>0) {
-                count+=(num%10);
-                num/=10;
+        int N = sc.nextInt();
+        int answer = N;
+        
+        for (int i=N-1; i>0; i--) {
+            String str = String.valueOf(i);
+            String[] arr = str.split("");
+            int num = 0;
+            for (int j=0; j<arr.length; j++) {
+                num+=Integer.parseInt(arr[j]);
             }
-            if (count==n) {
+            if (N==(i+num))
                 answer = i;
-                break;
-            }
         }
-        System.out.println(answer);
+        
+        if (answer==N)
+            answer=0;
+        
+        System.out.print(answer);
     }
 }
