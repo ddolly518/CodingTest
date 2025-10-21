8dd467fc6e99bb5b341782bd6e29c2dc9633ad48
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-22 01:42:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,19 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = n/5;
-        int num = -1;
-        for (int i=a; i>=0; i--) {
-            int b = n-5*i;
-            if (b%3==0) {
-                int c = b/3;
-                num = i+c;
-                break;
+        int N = sc.nextInt();
+        int answer = Integer.MAX_VALUE;
+        
+        int count = N/5;
+        for (int i=count; i>=0; i--) {
+            int num = N-i*5;
+            if (num%3==0) {
+                int sum = (i+num/3);
+                answer = Math.min(answer,sum);
             }
         }
-        System.out.println(num);
+        
+        answer = (answer==Integer.MAX_VALUE)?-1:answer;
+        System.out.print(answer);
     }
 }
