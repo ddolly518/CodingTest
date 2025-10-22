5458d62560b70bd61a92fee15f56a33733dd19ba
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-22 13:55:08 +0900

    [Silver V] Title: 소트인사이드, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -3,17 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int len = str.length();
-        int[] arr = new int[len];
-        int num = Integer.parseInt(str);
-        for (int i=0; i<len; i++) {
-            arr[i] = (num%10);
-            num/=10;
-        }
-        Arrays.sort(arr);
-        for (int i=len-1; i>=0; i--) {
-            System.out.print(arr[i]);
+        String str = sc.next();
+        String[] arr = str.split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        for (String s : arr) {
+            System.out.print(s);
         }
     }
 }
