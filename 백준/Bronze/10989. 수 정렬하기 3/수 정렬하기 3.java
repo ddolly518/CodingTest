df5ded1f58ce10518c529277fc45cc7b8234472b
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-22 13:11:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2556 ms, Memory: 345628 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,14 +7,15 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        
         for (int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
+        
         for (int i=0; i<N; i++) {
             bw.write(arr[i]+"\n");
         }
         bw.flush();
-        bw.close();
     }
 }
