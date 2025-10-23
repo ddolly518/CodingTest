25997282b7fc14f83640f17eb7bd0322beea3cdb
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-24 00:11:54 +0900

    [Silver II] Title: 좌표 압축, Time: 2220 ms, Memory: 281816 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        
+        Set<Integer> set = new HashSet<>();
         for (int i=0; i<N; i++) {
             int num = Integer.parseInt(st.nextToken());
             set.add(num);
@@ -17,14 +19,12 @@ public class Main {
         List<Integer> list = new ArrayList<>(set);
         Collections.sort(list);
         Map<Integer, Integer> map = new HashMap<>();
-        int index = 0;
-        for (int num : list) {
-            map.put(num, index++);
+        for (int i=0; i<list.size(); i++) {
+            map.put(list.get(i),i);
         }
         for (int i=0; i<N; i++) {
             bw.write(map.get(arr[i])+" ");
         }
         bw.flush();
-        bw.close();
     }
 }
