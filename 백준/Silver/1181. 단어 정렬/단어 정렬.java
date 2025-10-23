89764dc5e5b323c551548e1a457fc569bd9e4374
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-23 21:24:20 +0900

    [Silver V] Title: 단어 정렬, Time: 352 ms, Memory: 24580 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,21 +4,27 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
+        
         for (int i=0; i<N; i++) {
             set.add(br.readLine());
         }
+        
         List<String> list = new ArrayList<>(set);
-        Collections.sort(list, (a,b)-> {
-            if (a.length()==b.length()) {
-                return a.compareTo(b);
-            } else {
-                return Integer.compare(a.length(),b.length());
-            }
+        
+        Collections.sort(list, (a,b) -> {
+           if (a.length()==b.length()) {
+               return a.compareTo(b);
+           } else {
+               return a.length()-b.length();
+           }
         });
-        for (String s : list) {
-            System.out.println(s);
+        
+        for (String str : list) {
+            bw.write(str+"\n");
         }
+        bw.flush();
     }
 }
