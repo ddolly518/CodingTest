0dcf5fc7fc6fee8e0247d5d10f989f348ac34fde
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-24 16:35:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 700 ms, Memory: 53232 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -5,23 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int N = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
         for (int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String str = st.nextToken();
-            String s = st.nextToken();
-            if (s.equals("enter"))
+            String status = st.nextToken();
+            if (status.equals("enter")) {
                 set.add(str);
-            else if (s.equals("leave"))
+            } else {
                 set.remove(str);
+            }
         }
         List<String> list = new ArrayList<>(set);
-        Collections.sort(list);
-        for (int i=list.size()-1; i>=0; i--) {
+        Collections.sort(list, Collections.reverseOrder());
+        for (int i=0; i<list.size(); i++) {
             bw.write(list.get(i)+"\n");
         }
         bw.flush();
-        bw.close();
     }
 }
