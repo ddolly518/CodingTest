f1c17e7b31802a7cdf98748ae3aa9c7b7ff43142
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-27 16:32:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1100 ms, Memory: 173532 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,20 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Map<Integer, Integer> map = new HashMap<>();
+        
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        Map<Integer,Integer> map = new HashMap<>();
+        
         for (int i=0; i<N; i++) {
             int num = Integer.parseInt(st.nextToken());
             map.put(num, map.getOrDefault(num,0)+1);
         }
+        
         int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i=0; i<M; i++) {
-            int num = Integer.parseInt(st.nextToken()); 
-            bw.write(map.getOrDefault(num,0)+" ");
+            int num = Integer.parseInt(st.nextToken());
+            bw.write(map.getOrDefault(num,0)+"\n");
         }
+        
         bw.flush();
-        bw.close();
     }
 }
