0a5a01c16ac67784538c9cb7bd9c15e29f133370
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-28 18:13:32 +0900

    [Silver IV] Title: 스택 2, Time: 1124 ms, Memory: 239744 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -3,37 +3,37 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Stack<Integer> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        for (int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int num = Integer.parseInt(st.nextToken());
             switch (num) {
-                case 1: stack.push(Integer.parseInt(st.nextToken())); break;
-                case 2: if (stack.isEmpty()) {
+                case 1 : stack.push(Integer.parseInt(st.nextToken())); break;
+                case 2 : if (stack.isEmpty()) {
                     bw.write(-1+"\n");
                 } else {
                     bw.write(stack.pop()+"\n");
                 }
                     break;
-                case 3: bw.write(stack.size()+"\n"); break;
-                case 4: if (stack.isEmpty()) {
+                case 3 : bw.write(stack.size()+"\n"); break;
+                case 4 : if (stack.isEmpty()) {
                     bw.write(1+"\n");
                 } else {
                     bw.write(0+"\n");
                 }
                     break;
-                case 5: if (stack.isEmpty()) {
+                case 5 : if (stack.isEmpty()) {
                     bw.write(-1+"\n");
                 } else {
                     bw.write(stack.peek()+"\n");
                 }
+                    break;
             }
         }
         bw.flush();
-        bw.close();
     }
 }
