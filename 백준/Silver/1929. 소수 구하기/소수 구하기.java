24e5d40456bb01375259da344dffddb2480170f8
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-29 19:53:49 +0900

    [Silver III] Title: 소수 구하기, Time: 548 ms, Memory: 25784 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,18 +3,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        
-        if (n==1) {
-            n++;
-        }
-        
-        for (int i=n; i<=m; i++) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        for (int i=M; i<=N; i++) {
+            if (i<2) continue;
             boolean bo = true;
             for (int j=2; j*j<=i; j++) {
                 if (i%j==0) {
@@ -22,10 +16,10 @@ public class Main {
                     break;
                 }
             }
-            if (bo)
+            if (bo) {
                 bw.write(i+"\n");
+            }
         }
         bw.flush();
-        bw.close();
     }
 }
