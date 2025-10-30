7362a605bd5972e1d925d446784a07e375374fd3
KMS <80440158+ddolly518@users.noreply.github.com>
2025-10-30 18:30:19 +0900

    [Silver V] Title: 창문 닫기, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -3,14 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int num = 1;
-        while (true) {
-            if (num*num>n) {
-                System.out.println(num-1);
-                break;
-            }
-            num++;
-        }
+        long n = sc.nextLong();
+        System.out.println((int)Math.sqrt(n));
     }
 }
