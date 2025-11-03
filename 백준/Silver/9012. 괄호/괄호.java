0509155660613c5fc3b26edf6ec0323d06838962
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-03 21:05:26 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -5,32 +5,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         
-        int n = Integer.parseInt(br.readLine());
-        for (int i=0; i<n; i++) {
-            Stack<Character> stack = new Stack<>();
-            String st = br.readLine();
+        for (int i=0; i<N; i++) {
+            String[] arr = br.readLine().split("");
+            Stack<String> stack = new Stack<>();
             boolean bo = true;
-            for (int j=0; j<st.length(); j++) {
-                Character str = st.charAt(j);
-                if (str=='(')
-                    stack.push('(');
-                else if (!stack.isEmpty() && str==')')
-                    stack.pop();
-                else {
-                    bo = false;
-                    break;
+            
+            for (int j=0; j<arr.length; j++) {
+                String str = arr[j];
+                if (stack.isEmpty()) {
+                    stack.push(str);
+                } else {
+                    if (str.equals("(")) {
+                        stack.push("(");
+                    } else if (!(stack.pop()).equals("(")) {
+                        bo = false;
+                        break;
+                    }
                 }
             }
+            
             if (!stack.isEmpty())
                 bo = false;
-            if (bo) {
-                bw.write("YES"+"\n");
-            } else {
-                bw.write("NO"+"\n");
-            }
+            
+            if (bo)
+                bw.write("YES\n");
+            else
+                bw.write("NO\n");
         }
+        
         bw.flush();
-        bw.close();
     }
 }
