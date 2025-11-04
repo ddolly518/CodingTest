2b825824b096274bed3b5710ce52d3496f7a51db
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-04 19:22:52 +0900

    [Silver IV] Title: 큐 2, Time: 1328 ms, Memory: 331576 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -5,45 +5,42 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        Deque<Integer> queue = new LinkedList<>();
-        for (int i=0; i<n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String str = st.nextToken();
             if (str.equals("push")) {
-                queue.offer(Integer.parseInt(st.nextToken()));
+                dq.offer(Integer.parseInt(st.nextToken()));
             } else if (str.equals("pop")) {
-                if (queue.isEmpty()) {
+                if (dq.isEmpty()) {
                     bw.write(-1+"\n");
                 } else {
-                    bw.write(queue.poll()+"\n");
+                    bw.write(dq.poll()+"\n");
                 }
             } else if (str.equals("size")) {
-                bw.write(queue.size()+"\n");
-                
+                bw.write(dq.size()+"\n");
             } else if (str.equals("empty")) {
-                if (queue.isEmpty()) {
+                if (dq.isEmpty()) {
                     bw.write(1+"\n");
                 } else {
                     bw.write(0+"\n");
                 }
-                
             } else if (str.equals("front")) {
-                if (queue.isEmpty()) {
+                if (dq.isEmpty()) {
                     bw.write(-1+"\n");
                 } else {
-                    bw.write(queue.peek()+"\n");
+                    bw.write(dq.peek()+"\n");
                 }
-                
             } else if (str.equals("back")) {
-                if (queue.isEmpty()) {
+                if (dq.isEmpty()) {
                     bw.write(-1+"\n");
                 } else {
-                    bw.write(queue.peekLast()+"\n");
+                    bw.write(dq.peekLast()+"\n");
                 }
             }
         }
         bw.flush();
-        bw.close();
     }
 }
