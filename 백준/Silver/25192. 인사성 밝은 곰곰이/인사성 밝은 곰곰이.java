ec57a67ab01fd82c9d85615b6e6aa793d5081e95
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-11 20:15:37 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 256 ms, Memory: 28952 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -4,18 +4,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
         int n = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
-        int count = 0;
         for (int i=0; i<n; i++) {
             String str = br.readLine();
             if (str.equals("ENTER")) {
-                set.clear();
+                set = new HashSet<>();
             } else if (!set.contains(str)) {
                 set.add(str);
-                count++;
+                answer++;
             }
         }
-        System.out.println(count);
+        System.out.print(answer);
     }
 }
