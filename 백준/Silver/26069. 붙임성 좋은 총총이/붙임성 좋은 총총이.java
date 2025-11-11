434a94d346b8dfe66ec9b73c35e927c118b3f8d9
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-11 20:32:07 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 124 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -4,24 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 1;
-        Set<String> set = new HashSet<>();
+        int answer = 1;
         int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
         set.add("ChongChong");
+        
         for (int i=0; i<n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String a = st.nextToken();
             String b = st.nextToken();
-            
             if (set.contains(a) && !set.contains(b)) {
-                count++;
                 set.add(b);
-            }
-            if (!set.contains(a) && set.contains(b)) {
-                count++;
+                answer++;
+            } else if (!set.contains(a) && set.contains(b)) {
                 set.add(a);
+                answer++;
             }
         }
-        System.out.println(count);
+        
+        System.out.print(answer);
     }
 }
