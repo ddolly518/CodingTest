c6ac06954afbfe398b1bb0da23662d335b7464eb
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-11 19:57:42 +0900

    [Bronze I] Title: 약수, Time: 112 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -4,16 +4,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for (int i=0; i<n; i++) {
-            int m = Integer.parseInt(st.nextToken());
-            if (min>m) min = m;
-            if (max<m) max = m;
+        
+        if (n==1) {
+            int num = Integer.parseInt(br.readLine());
+            min = num;
+            max = num;
+        } else {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int i=0; i<n; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                min = Math.min(min,num);
+                max = Math.max(max,num);
+            }
         }
-        System.out.println(min*max);
+        
+        System.out.print(min*max);
     }
 }
