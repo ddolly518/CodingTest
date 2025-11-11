d8f5ff7060726f730272c1d02e3fe73e8fbe9292
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-11 18:46:25 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -4,14 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n==0 || n==1) {
-            System.out.println(1);
-            return;
-        }
-        int num = 1;
-        for (int i=2; i<=n; i++) {
-            num*=i;
-        }
-        System.out.println(num);
+        System.out.print(fibo(n));
+    }
+    private static int fibo(int n) {
+        if (n==0) return 1;
+        return n*fibo(n-1);
     }
 }
