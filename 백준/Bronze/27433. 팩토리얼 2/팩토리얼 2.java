3c124c9c24b28ccf7af08d3e3f596edad077e2b4
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-12 15:24:46 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 184 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,15 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(fibo(n));
+        System.out.print(factory(n));
     }
-    public static long fibo(int n) {
-        if (n==0 || n==1) {
-            return 1;
-        }
-        return n*fibo(n-1);
+    private static long factory(int n) {
+        if (n<2) return 1;
+        return n*factory(n-1);
     }
 }
