3cc02528d623d60dd662895ed96348271c2131b6
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-17 17:25:29 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,25 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int[] arr = new int[21];
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        fibo(n);
-        System.out.println(arr[n]);
-        
+        System.out.print(fibo(n));
     }
-    public static int fibo(int n) {
-        if (n==0) {
-            arr[0] = 0;
-            return 0;
-        }
-        if (n==1) {
-            arr[1] = 1;
-            return 1;
-        }
-        if (arr[n] != 0) return arr[n];
-        return arr[n] = fibo(n-1) + fibo(n-2);
+    private static int fibo(int n) {
+        if (n<2) return n;
+        return fibo(n-1)+fibo(n-2);
     }
 }
