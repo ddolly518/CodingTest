09c5cc5ce0d4bb6f77c0baafc2ac5396677e77cd
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-22 11:16:15 +0900

    [Silver II] Title: 통계학, Time: 636 ms, Memory: 64944 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,30 +4,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        long sum = 0;
-        List<Integer> list = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i<n; i++) {
+        int[] arr = new int[N];
+        double sum = 0;
+        
+        for (int i=0; i<N; i++) {
             int num = Integer.parseInt(br.readLine());
-            sum+=num;
-            list.add(num);
+            arr[i] = num;
             map.put(num, map.getOrDefault(num,0)+1);
+            sum+=num;
         }
-        Collections.sort(list);
-        int maxFreq = Collections.max(map.values());
-        List<Integer> freq = new ArrayList<>();
+        Arrays.sort(arr);
+        
+        int max = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue()==maxFreq) {
-                freq.add(entry.getKey());
+            max = Math.max(entry.getValue(), max);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue()==max) {
+                list.add(entry.getKey());
             }
         }
-        Collections.sort(freq);
-        int num = (freq.size()==1)?freq.get(0):freq.get(1);
-        System.out.println(Math.round((double) sum / n));
-        System.out.println(list.get(list.size()/2));
-        System.out.println(num);
-        System.out.println(list.get(list.size()-1)-list.get(0));
+        Collections.sort(list);
+        int value = 0;
+        if (list.size()==1) {
+            value = list.get(0); 
+        } else {
+            value = list.get(1);
+        }
+        
+        System.out.println((int)Math.round(sum/N));
+        System.out.println(arr[N/2]);
+        System.out.println(value);
+        System.out.println(arr[N-1]-arr[0]);
     }
 }
