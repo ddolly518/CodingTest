b684d5d976bcbb270e0f880564d3539d41b4b5a9
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-24 17:46:38 +0900

    [Silver III] Title: N과 M (4), Time: 232 ms, Memory: 20596 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] visited;
+    static BufferedWriter bw;
+    
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+        visited = new boolean[N+1];
+        
+        backtracking(0);
+        
+        bw.flush();
+    }
+    
+    private static void backtracking(int length) throws IOException {
+        if (length==M) {
+            print();
+            return;
+        }
+        
+        for (int i=1; i<=N; i++) {
+            if (length==0 || length>0 && arr[length-1]<=i) {
+                visited[i] = true;
+                arr[length] = i;
+                backtracking(length+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    private static void print() throws IOException {
+        for (int i=0; i<M; i++) {
+            bw.write(arr[i] + " ");
+        }
+        bw.newLine();
+    }
+}
