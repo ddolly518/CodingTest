2e4faa1ede257aa0b22b96e62b2ec4792b5bf732
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-26 17:02:45 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1640 ms, Memory: 314440 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -1,55 +1,62 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static int[] arr, tmp;
-    static int count = 0;
-    static int k, result = -1;
+    static int N;
+    static int K; 
+    static int[] A;
+    static int count;
+    static int[] tmp;
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        arr = new int[n];
-        tmp = new int[n];
-
-        for (int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        A = new int[N+1];
+        tmp = new int[N+1];
+        count = 0;
+        
+        for (int i=1; i<=N; i++) {
+            A[i] = sc.nextInt();
         }
-        merge_sort(arr, 0, n-1);
-        System.out.println(result);
+        
+        merge_sort(1,N);
+        if (count < K) {
+            System.out.print(-1);
+        }
     }
     
-    public static void merge_sort(int[] A, int p, int r) { 
+    private static void merge_sort(int p, int r) { 
         if (p < r) {
             int q = (p + r) / 2;      
-            merge_sort(A, p, q);     
-            merge_sort(A, q + 1, r); 
-            merge(A, p, q, r);        
+            merge_sort(p, q);    
+            merge_sort(q + 1, r);  
+            merge(p, q, r);      
         }
     }
-
-    public static void merge(int[] A, int p, int q, int r) {
-        int i = p; 
+    
+    private static void merge(int p, int q, int r) {
+        int i = p;
         int j = q + 1;
-        int t = 0;
+        int t = 1;
+        
         while (i <= q && j <= r) {
-            if (A[i] <= A[j]) tmp[t++] = A[i++]; 
-            else tmp[t++] = A[j++]; 
+            if (A[i] <= A[j])
+                tmp[t++] = A[i++];
+            else 
+                tmp[t++] = A[j++];
         }
         while (i <= q)  
             tmp[t++] = A[i++];
-        while (j <= r)  
+        while (j <= r)
             tmp[t++] = A[j++];
-        i = p;
-        t = 0;
+        i = p; 
+        t = 1;
         while (i <= r) {
-            A[i] = tmp[t++];
             count++;
-            if (count == k) result = A[i];
-            i++;
+            A[i++] = tmp[t++]; 
+            if (count == K) {
+                System.out.print(A[i-1]);
+            }
         }
     }
 }
