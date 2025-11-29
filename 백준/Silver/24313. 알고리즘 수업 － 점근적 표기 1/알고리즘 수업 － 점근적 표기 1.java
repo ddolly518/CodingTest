bbaf0b87a72ab7ed1122a3e8c75ea69a6d15b7f1
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-29 17:47:10 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,24 +1,25 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a1 = Integer.parseInt(st.nextToken());
-        int a0 = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(br.readLine());
-        int n0 = Integer.parseInt(br.readLine());
-        boolean valid = true;
-        for (int n = n0; n <= 100; n++) {
-            long fn = (long)a1 * n + a0;
-            long cn = (long)c * n;
-            if (fn > cn) {
-                valid = false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a0 = sc.nextInt();
+        int c = sc.nextInt();
+        int n0 = sc.nextInt();
+        boolean bo = true;
+        
+        for (int i=n0; i<=100; i++) {
+            if (a1*i+a0>c*i) {
+                bo = false;
                 break;
             }
         }
-
-        System.out.println(valid ? 1 : 0);
+        
+        if (bo) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
     }
 }
