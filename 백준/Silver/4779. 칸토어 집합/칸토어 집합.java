cb96b7050ab45a713b05a21be296f3be6700d701
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-29 14:36:44 +0900

    [Silver III] Title: 칸토어 집합, Time: 240 ms, Memory: 24572 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,30 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static BufferedWriter bw;
+    static StringBuilder sb;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String line;
+        
         while ((line=br.readLine())!=null) {
-            int n = Integer.parseInt(line);
-            int num = (int) Math.pow(3,n);
-            bw.write(fun(1,num)+"\n");
+            sb = new StringBuilder();
+            int num = Integer.parseInt(line);
+            fun(0, (int)Math.pow(3,num));
+            bw.write(sb.toString()+"\n");
         }
+        
         bw.flush();
-        bw.close();
     }
-    private static String fun(int a, int b) {
-        StringBuilder sb = new StringBuilder();
-        int num = b-a+1;
+    
+    private static void fun(int start, int end) {
+        int num = end-start;
+        
         if (num==1) {
             sb.append("-");
-        } else {
-            int len = num/3;
-            sb.append(fun(a, a+len-1));
-            String s = " ";
-            sb.append(s.repeat(len));
-            sb.append(fun(b-len+1, b));
+            return;
         }
-        return sb.toString();
+        
+        fun(start,start+num/3);
+        for (int i=0; i<num/3; i++) {
+            sb.append(" ");
+        }
+        fun(start+num/3*2,end);
     }
 }
