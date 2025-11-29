8e627e4280a03a840e32c305566035602b603477
KMS <80440158+ddolly518@users.noreply.github.com>
2025-11-29 17:31:33 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 164 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt());
-        System.out.println(1);
+        int n = sc.nextInt();
+        System.out.println(n);
+        System.out.print(1);
     }
 }
