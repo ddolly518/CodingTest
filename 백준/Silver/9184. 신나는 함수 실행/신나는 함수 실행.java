f6882ea3759b7eb191466495bdc3f850351d4d17
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-05 17:20:47 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 300 ms, Memory: 26748 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -2,37 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static int[][][] w = new int[105][105][105];
+    static int[][][] w;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        w = new int[21][21][21];
         
-        while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            if (a==-1 && b==-1 && c==-1) {
+            
+            if (a==-1 && b==-1 && c==-1)
                 break;
-            }
-            bw.write("w("+a+", "+b+", "+c+") = "+fun(a,b,c)+"\n");
+            
+            bw.write("w("+a+", "+b+", "+c+") = "+w(a,b,c)+"\n");
         }
+        
         bw.flush();
-        bw.close();
     }
-    private static int fun(int a, int b, int c) {
+    
+    private static int w(int a, int b, int c) {
         if (a<=0 || b<=0 || c<=0) {
             return 1;
-        }
-        if (a>20 || b>20 || c>20) {
-            return fun(20,20,20);
-        }
-        if (w[a][b][c]!=0) return w[a][b][c];
+        } else if (a>20 || b>20 || c>20) {
+            return w(20,20,20);
+        } 
+        
+        if (w[a][b][c]!=0) 
+            return w[a][b][c];
+        
         if (a<b && b<c) {
-            w[a][b][c] = fun(a,b,c-1)+fun(a,b-1,c-1)-fun(a,b-1,c);
+            w[a][b][c] = w(a,b,c-1)+w(a,b-1,c-1)-w(a,b-1,c);
         } else {
-            w[a][b][c] = fun(a-1,b,c)+fun(a-1,b-1,c)+fun(a-1,b,c-1)-fun(a-1,b-1,c-1);
-        }
+            w[a][b][c] = w(a-1,b,c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1);
+        }            
+        
         return w[a][b][c];
     }
 }
