25085b85c2d22d8dda51af9b74db05792b1d4779
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-11 23:12:06 +0900

    [Bronze II] Title: 저항, Time: 184 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -1,20 +1,32 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> map = new HashMap<>();
-        String[] arr = new String[]{"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
-        for (int i=0; i<10; i++) {
-            map.put(arr[i],i);
+    public static void main(String[] args) {
+        String[] arr = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        
+        Scanner sc = new Scanner(System.in);
+        
+        long num = 0;
+        
+        for (int i=0; i<3; i++) {
+            String str = sc.next();
+            int count = 0;
+            for (int j=0; j<10; j++) {
+                if (arr[j].equals(str)) {
+                    count = j;
+                    break;
+                }
+            }
+            if (i==2) {
+                num*=(int)Math.pow(10,count);
+            } else if (i==1) {
+                num+=count;
+            } else {
+                num+=count;
+                num*=10;
+            }
         }
-        String one = br.readLine();
-        String two = br.readLine();
-        String three = br.readLine();
-        long num = map.get(one)*10;
-        num+=map.get(two);
-        num*=((long)Math.pow(10,map.get(three)));
-        System.out.println(num);
+        
+        System.out.print(num);
     }
 }
