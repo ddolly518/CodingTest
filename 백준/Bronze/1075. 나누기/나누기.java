8c28ed67bc4a7613eed98c1aae9d1bbbcd55631f
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-11 23:02:15 +0900

    [Bronze II] Title: 나누기, Time: 168 ms, Memory: 18224 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.java
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int F = sc.nextInt();
-        int num = (N/100)*100;
+        int n = sc.nextInt()/100*100;
+        int f = sc.nextInt();
+        
         for (int i=0; i<100; i++) {
-            int sum = num+i;
-            if (sum%F==0) {
-                System.out.printf("%02d",i);
+            if ((n+i)%f==0) {
+                if (i<10) {
+                    System.out.print("0"+i);
+                } else {
+                    System.out.print(i);
+                }
                 break;
             }
         }
