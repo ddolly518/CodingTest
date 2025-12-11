3f5ec04eb12662159d51af4438c51124b468bddd
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-11 22:46:53 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -5,21 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
         String str = br.readLine();
-        Character[] arr = new Character[str.length()];
+        char[] arr = new char[str.length()];
         for (int i=0; i<str.length(); i++) {
             char ch = str.charAt(i);
             arr[i] = ch;
         }
+        
         for (int i=1; i<n; i++) {
             String s = br.readLine();
-            for (int j=0; j<s.length(); j++) {
+            for (int j=0; j<str.length(); j++) {
                 char ch = s.charAt(j);
-                if (ch!='?' && arr[j]!=ch) {
-                    arr[j] = '?';
+                if (arr[j]!=ch) {
+                    arr[j]='?';
                 }
             }
         }
+        
         for (int i=0; i<str.length(); i++) {
             System.out.print(arr[i]);
         }
