c84553878606086d66ca4f1a444b1af870604ca2
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-11 22:37:49 +0900

    [Bronze II] Title: 분산처리, Time: 964 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int n = Integer.parseInt(br.readLine());
         for (int i=0; i<n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int num = 1;
-            for (int j=0; j<b; j++) {
-                num = (num*a)%10;
+            
+            int num = a;
+            for (int j=1; j<b; j++) {
+                num*=a;
+                num%=10;
             }
-            if (num==0) num=10;
+            
+            num = (num%10==0)?10:num%10;
             bw.write(num+"\n");
         }
         bw.flush();
-        bw.close();
     }
 }
