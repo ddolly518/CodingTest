6e367c66c7886057d2cb991d5dfcd299807cdfdf
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-12 22:58:11 +0900

    [Silver I] Title: 절댓값 힙, Time: 324 ms, Memory: 26164 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> {
+            if (Math.abs(a)==Math.abs(b)) {
+                return a-b;
+            } else {
+                return Math.abs(a)-Math.abs(b);
+            }
+        });
+        
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    bw.write(0+"\n");
+                } else {
+                    bw.write(queue.poll()+"\n");
+                }
+            } else {
+                queue.offer(num);
+            }
+        }
+        bw.flush();
+    }
+}
