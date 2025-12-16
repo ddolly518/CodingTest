f39b84fe2a48b338b2cffb789c1d2951d88c1d15
KMS <80440158+ddolly518@users.noreply.github.com>
2025-12-16 16:05:30 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
+        
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0)
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            if (i % 3 == 0)
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+        }
+        
+        System.out.print(arr[n]);
+    }
+
+}
