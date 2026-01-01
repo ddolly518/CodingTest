46ce691b8ff01cdd6dff4775c040231519412100
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-01 21:24:48 +0900

    [Silver III] Title: 수열, Time: 252 ms, Memory: 24680 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -4,27 +4,25 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr;
-        int[] S;
-        int max = Integer.MIN_VALUE;
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        
-        arr = new int[n+1];
-        S = new int[n+1];
-        
+        int[] arr = new int[N+1];
+        int[] sum = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        for (int i=1; i<n+1; i++) {
+        for (int i=1; i<=N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            S[i] = S[i-1]+arr[i];
+            sum[i] = sum[i-1] + arr[i];
         }
         
-        for (int i=1; i<=n+1-m; i++) {
-            int num = S[i+m-1]-S[i-1];
-            max = (max<num)?num:max;
+        int max = Integer.MIN_VALUE;
+        for (int i=K; i<=N; i++) {
+            int num = sum[i]-sum[i-K];
+            max = Math.max(max,num);
         }
-        System.out.println(max);
+        bw.write(max+"");
+        bw.flush();
     }
 }
