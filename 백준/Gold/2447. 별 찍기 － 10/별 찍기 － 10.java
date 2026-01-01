d82deb3bc62967004f6717b9435f39ba1b49abeb
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-01 18:55:40 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 396 ms, Memory: 41472 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -2,46 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static char[][] arr;
+    static String[][] arr;
     
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = sc.nextInt();
-        arr = new char[n][n];
+        int n = Integer.parseInt(br.readLine());
+        arr = new String[n][n];
         
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(arr[i], '*');
+        for (int i=0; i<n; i++) {
+            Arrays.fill(arr[i], " ");
         }
         
-        fun(0,0,n);
+        f(n, 0, 0);
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                bw.write(arr[i][j]);
+                bw.write(arr[i][j]+"");
             }
-            bw.newLine();
+            bw.write("\n");
         }
+        
         bw.flush();
     }
     
-    private static void fun(int x, int y, int len) {
-        if (len==1) {
+    private static void f(int length, int x, int y) {
+        if (length == 1) {
+            arr[x][y] = "*";
             return;
         }
-        int num = len/3;
-        fun(x, y, num);
-        fun(x+num, y, num);
-        fun(x+2*num, y, num);
-        fun(x, y+num, num);
-        for (int i=0; i<num; i++) {
-            for (int j=0; j<num; j++) {
-                arr[x+num+i][y+num+j] = ' ';
-            }
-        }
-        fun(x+2*num, y+num, num);
-        fun(x, y+2*num, num);
-        fun(x+num, y+2*num, num);
-        fun(x+2*num, y+2*num, num);
+        
+        int num = length/3;
+        f(num, x, y);
+        f(num, x+num, y);
+        f(num, x+2*num, y);
+        f(num, x, y+num);
+        f(num, x+2*num, y+num);
+        f(num, x, y+2*num);
+        f(num, x+num, y+2*num);
+        f(num, x+2*num, y+2*num);
     }
 }
