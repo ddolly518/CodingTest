29bbddfa8a98ed971c6148afb149f9de8f67a00a
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-11 19:49:36 +0900

    [Bronze V] Title: A+B - 2, Time: 160 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        System.out.print(A+B);
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A+B);
     }
 }
