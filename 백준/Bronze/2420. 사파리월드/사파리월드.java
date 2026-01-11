92d3a5552ed45ce53465cb3dd2252a2cdf0a61ed
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-11 19:48:16 +0900

    [Bronze V] Title: 사파리월드, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.java
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -1,12 +1,10 @@
 import java.util.*;
-import java.math.BigInteger;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        BigInteger A = new BigInteger(in.next());
-        BigInteger B = new BigInteger(in.next());
-         System.out.println((A.subtract(B)).abs());
-        
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+        System.out.println(Math.abs(N-M));
     }
 }
