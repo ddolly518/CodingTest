876de16b38ba7ac9df3d2fd5393205467f686c6e
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-24 19:32:45 +0900

    [Bronze V] Title: Zadanie próbne, Time: 164 ms, Memory: 17512 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(sc.next());
+        System.out.println(sb.reverse().toString());
+    }
+}
