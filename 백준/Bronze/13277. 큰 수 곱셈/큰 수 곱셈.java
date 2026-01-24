f61de0b509d37494ca22bb379911a402db39e1bf
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-24 20:28:14 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5548 ms, Memory: 398332 KB -BaekjoonHub

--- 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -4,12 +4,8 @@ import java.math.BigInteger;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        BigInteger a = new BigInteger(sc.next());
-        BigInteger b = new BigInteger(sc.next());
-        
-        BigInteger mult = a.multiply(b);
-        System.out.println(mult);
-        sc.close();
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.print(a.multiply(b));
     }
 }
