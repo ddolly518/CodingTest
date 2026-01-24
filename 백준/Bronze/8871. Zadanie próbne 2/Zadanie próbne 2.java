aa56dec258b20caf0e5ce863fe346161042fde63
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-24 19:42:17 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 188 ms, Memory: 18468 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 3*(n+1);
+        System.out.println(2*(n+1)+" "+num);
+    }
+}
