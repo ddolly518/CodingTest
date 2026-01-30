547dec82e990089023f74c2e1b85b0160fb6cab5
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-30 22:24:04 +0900

    [Bronze V] Title: A+B - 9, Time: 212 ms, Memory: 17516 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -5,11 +5,11 @@ import java.math.BigInteger;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         BigInteger a = new BigInteger(st.nextToken());
         BigInteger b = new BigInteger(st.nextToken());
-        
-        BigInteger sum = a.add(b);
-        System.out.println(sum);
+        bw.write(a.add(b).toString()+"");
+        bw.flush();
     }
 }
