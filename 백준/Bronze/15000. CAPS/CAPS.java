c349d07c7ea7e325e8c97f0f2302159c7187f054
KMS <80440158+ddolly518@users.noreply.github.com>
2026-01-30 22:06:34 +0900

    [Bronze V] Title: CAPS, Time: 3548 ms, Memory: 110832 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        for (int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            System.out.print(Character.toUpperCase(ch));
+        }
+    }
+}
