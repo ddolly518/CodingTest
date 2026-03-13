6b456fc7ba4f429e27804e7de5760a04e1b866f0
KMS <80440158+ddolly518@users.noreply.github.com>
2026-03-13 20:13:48 +0900

    [Silver II] Title: 최대 힙, Time: 320 ms, Memory: 27312 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -5,24 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> b-a);
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        int n = Integer.parseInt(br.readLine());
-        
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<N; i++) {
             int num = Integer.parseInt(br.readLine());
-            
             if (num == 0) {
-                if (queue.isEmpty()) {
-                    bw.write(0+"\n");
+                if (pq.isEmpty()) {
+                    bw.write("0"+"\n");
                 } else {
-                    bw.write(queue.poll()+"\n");
+                    bw.write(pq.poll()+"\n");
                 }
             } else {
-                queue.offer(num);
+                pq.offer(num);
             }
         }
-        
         bw.flush();
+        bw.close();
     }
 }
