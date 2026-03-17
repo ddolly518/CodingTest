4a5abf7daad5a2c1f8f9a624bec180deff628684
KMS <80440158+ddolly518@users.noreply.github.com>
2026-03-17 14:14:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 964 ms, Memory: 99244 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static List<Integer>[] A;
+    static boolean[] visited;
+    static int[] count;
+    static int cnt;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        A = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        count = new int[N+1];
+        cnt = 1;
+        
+        for (int i=0; i<=N; i++) {
+        	A[i] = new ArrayList<Integer>();
+        }
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[a].add(b);
+            A[b].add(a);
+        }
+        for (int i=1; i<=N; i++) {
+            Collections.sort(A[i]);
+        }
+
+        dfs(R);
+        
+        for (int i=1; i<=N; i++) {
+            bw.write(count[i]+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    private static void dfs(int v) {
+        visited[v] = true;
+        count[v] = cnt++;
+        
+        for (int next : A[v]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
