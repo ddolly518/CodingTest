ccf6e39bd148e64c171040d21d183d287b29bfff
KMS <80440158+ddolly518@users.noreply.github.com>
2026-05-31 00:01:26 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,13 +1,12 @@
 class Solution {
     public String solution(int a, int b) {
-        Integer[] arr = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-        int sum = 0;
+        int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        
+        int sum = b-1;
         for (int i=0; i<a-1; i++) {
-            sum+=arr[i];
+            sum += month[i];
         }
-        sum+=(b-1);
-        String answer = day[sum%7];
-        return answer;
+        return day[sum%7];
     }
 }
