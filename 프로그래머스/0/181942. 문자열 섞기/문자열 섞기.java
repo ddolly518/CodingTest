14125f12fe57dd70c05d0fcac1301749cea14121
KMS <80440158+ddolly518@users.noreply.github.com>
2026-08-22 21:37:18 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,14 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<str1.length(); i++) {
-            answer+=String.valueOf(str1.charAt(i));
-            answer+=String.valueOf(str2.charAt(i));
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(i);
+            sb.append(ch1);
+            sb.append(ch2);
         }
+        answer = sb.toString();
         return answer;
     }
 }
