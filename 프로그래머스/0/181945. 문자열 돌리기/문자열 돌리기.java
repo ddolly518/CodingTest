ab6d4b5b168cb94d7a750e264d78902bf69903d3
KMS <80440158+ddolly518@users.noreply.github.com>
2026-08-22 17:30:33 +0900

    [level 0] Title: 문자열 돌리기, Time: 92.25 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -5,8 +5,8 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         
-        for (int i=0; i<a.length(); i++) {
-            System.out.println(a.charAt(i));
+        for (char ch : a.toCharArray()) {
+            System.out.println(ch);
         }
     }
 }
