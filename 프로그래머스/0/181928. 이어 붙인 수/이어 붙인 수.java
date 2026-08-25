02b3bf70d373b5d1573bb95d4c50f8603180adbf
KMS <80440158+ddolly518@users.noreply.github.com>
2026-08-25 21:59:53 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        String a = "";
-        String b = "";
-        for (int i : num_list) {
-            if (i%2==0) {
-                a+=(""+i);
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i=0; i<num_list.length; i++) {
+            int num = num_list[i];
+            if (num%2 == 0) {
+                sb1.append(num);
             } else {
-                b+=(""+i);
+                sb2.append(num);
             }
         }
-        answer+=(Integer.valueOf(a)+Integer.valueOf(b));
+        answer = Integer.valueOf(sb1.toString()) + Integer.valueOf(sb2.toString());
         return answer;
     }
 }
