49381b87a5084e4c160ecb6fe339c8765df5fe18
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-01 22:07:41 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,13 +1,9 @@
-import java.util.*;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[end_num-start_num+1];
+        int index = 0;
         for (int i=start_num; i<=end_num; i++) {
-            list.add(i);
-        }
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[i]=list.get(i);
+            answer[index++] = i;
         }
         return answer;
     }
