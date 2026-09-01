bc6c6543a3ad5f2c0a67dbcba68e73eb56d2dde0
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-01 22:52:52 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.06 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -3,52 +3,45 @@ import java.util.*;
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int answer = 0;
-        int[] dice = {a,b,c,d};
         Map<Integer, Integer> map = new HashMap<>();
-        for (int n : dice) {
-            map.put(n, map.getOrDefault(n,0)+1);
-        }
-        if (map.size()==1) {
-            answer=1111*a;
-        } else if (map.size()==2 && map.containsValue(2)) {
-            int p=0;
-            int q=0;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (p==0)
-                    p = entry.getKey();
-                else
-                    q = entry.getKey();
-            }
-            answer = (p+q)*Math.abs(p-q);
-        } else if (map.size()==2) {
-            int p=0;
-            int q=0;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (entry.getValue()==3)
-                    p = entry.getKey();
-                else if (entry.getValue()==1)
-                    q = entry.getKey();
-            }
-            answer = (int)Math.pow((10*p+q),2);
-        } else if (map.size()==3) {
+        map.put(a, map.getOrDefault(a,0)+1);
+        map.put(b, map.getOrDefault(b,0)+1);
+        map.put(c, map.getOrDefault(c,0)+1);
+        map.put(d, map.getOrDefault(d,0)+1);
+        
+        if (map.size() == 1) {
+            answer = 1111 * a;
+        } else if (map.size() == 2) {
+            int p = 0;
             int q = 0;
-            int r = 0;
+            boolean bo = true;
             for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (entry.getValue()==1) {
-                    if (q==0)
+                if (entry.getValue() == 1) {
+                    q = entry.getKey();
+                } else if (entry.getValue() == 3) {
+                    p = entry.getKey();
+                } else if (entry.getValue() == 2) {
+                    bo = false;
+                    if (p == 0) {
+                        p = entry.getKey();
+                    } else {
                         q = entry.getKey();
-                    else
-                        r = entry.getKey();
+                    }
                 }
             }
-            answer = q*r;
-        } else {
-            int min = 6;
-            for (int n : dice) {
-                if (min > n)
-                    min = n;
+            
+            answer = bo ? (int)Math.pow((10*p+q), 2) : (p+q) * Math.abs(p-q);
+        } else if (map.size() == 3) {
+            answer = 1;
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == 1) {
+                    answer*=entry.getKey();
+                }
             }
-            answer = min;
+        } else if (map.size() == 4) {
+            answer = Math.min(a,b);
+            answer = Math.min(answer, c);
+            answer = Math.min(answer, d);
         }
         return answer;
     }
