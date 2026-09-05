e4f0a3beb4c6f7fee4f240f63ec0e96e175232a1
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-05 14:31:46 +0900

    [level 0] Title: 접미사 배열, Time: 2.17 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,11 +1,17 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
+        String[] answer = {};
+        List<String> list = new ArrayList<>();
         for (int i=0; i<my_string.length(); i++) {
-            answer[i] = my_string.substring(i);
+            list.add(my_string.substring(i));
         }
-        Arrays.sort(answer);
+        Collections.sort(list);
+        answer = new String[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
