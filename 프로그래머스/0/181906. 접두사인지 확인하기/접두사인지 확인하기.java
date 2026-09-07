ca4b787f8ba259a9d31c60c3e9bbd5f96ef7bd5f
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-07 20:52:20 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.10 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        if (my_string.startsWith(is_prefix))
-            answer = 1;
+        List<String> list = new ArrayList<>();
+        for (int i=0; i<my_string.length(); i++) {
+            list.add(my_string.substring(0,i));
+        }
+        answer = (list.contains(is_prefix) ? 1 : 0);
         return answer;
     }
 }
