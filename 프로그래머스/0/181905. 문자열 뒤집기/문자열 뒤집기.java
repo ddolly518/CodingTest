8d25e03c97a9853ccc31d3109680fc23640cff22
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-07 20:59:39 +0900

    [level 0] Title: 문자열 뒤집기, Time: 5.36 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,13 +1,9 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        StringBuilder reverse = new StringBuilder(my_string.substring(s,e+1));
-        reverse.reverse();
-        sb.append(my_string.substring(0,s));
-        sb.append(reverse);
-        sb.append(my_string.substring(e+1));
-        answer = sb.toString();
+        StringBuilder sb = new StringBuilder(my_string.substring(s,e+1));
+        sb.reverse();
+        answer = my_string.substring(0,s) + sb.toString() + my_string.substring(e+1);
         return answer;
     }
 }
