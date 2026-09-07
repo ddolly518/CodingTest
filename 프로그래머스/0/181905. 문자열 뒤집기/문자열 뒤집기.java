6ea46d99a7ebb4cd43bd17fe448e8742be624b2d
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-07 20:58:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
-        StringBuilder sb = new StringBuilder(my_string);
-        while (s < e) {
-            char ch = my_string.charAt(s);
-            sb.setCharAt(s, my_string.charAt(e));
-            sb.setCharAt(e, ch);
-            s++;
-            e--;
-        }
+        StringBuilder sb = new StringBuilder();
+        StringBuilder reverse = new StringBuilder(my_string.substring(s,e+1));
+        reverse.reverse();
+        sb.append(my_string.substring(0,s));
+        sb.append(reverse);
+        sb.append(my_string.substring(e+1));
         answer = sb.toString();
         return answer;
     }
