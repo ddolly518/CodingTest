701eb4a07135d7f1a79c9aebf031174ec8699a31
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-10 22:35:20 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,33 +1,28 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        switch(n) {
-            case 1 :
-                for (int i=0; i<=slicer[1]; i++) {
-                    list.add(num_list[i]);
-                }
-                break;
-            case 2 :
-                for (int i=slicer[0]; i<num_list.length; i++) {
-                    list.add(num_list[i]);
-                }
-                break;
-            case 3 :
-                for (int i=slicer[0]; i<=slicer[1]; i++) {
-                    list.add(num_list[i]);
-                }
-                break;
-            case 4 :
-                for (int i=slicer[0]; i<=slicer[1]; i+=slicer[2]) {
-                    list.add(num_list[i]);
-                }
-                break;               
-        }
+        int[] answer = {};
         int index = 0;
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[index++] = list.get(i);
+        int start = 0;
+        int end = 0;
+        int num = 1;
+        if (n == 1) {
+            end = slicer[1];
+        } else if (n == 2) {
+            start = slicer[0];
+            end = num_list.length-1;
+        } else if (n == 3) {
+            start = slicer[0];
+            end = slicer[1];
+        } else if (n == 4) {
+            start = slicer[0];
+            end = slicer[1];
+            num = slicer[2];
+        }
+        answer = new int[1+(end-start)/num];
+        for (int i=start; i<=end; i+=num) {
+            answer[index++] = num_list[i];
         }
         return answer;
     }
