ff19921014db94a3f0127950c30912a2badb5826
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-10 21:38:24 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,7 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num-end_num+1];
+        int[] answer = new int[1+start_num-end_num];
         int index = 0;
         for (int i=start_num; i>=end_num; i--) {
             answer[index++] = i;
