eb1bf17a0e2ea405793d93c3434938f41a2acbf0
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-10 21:22:17 +0900

    [level 0] Title: 글자 지우기, Time: 1.07 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,15 +1,18 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        for (int i=0; i<my_string.length(); i++) {
-            Boolean b = true;
-            for (int num : indices) {
-                if (i==num)
-                    b = false;
-            }
-            if (b)
-                answer+=String.valueOf(my_string.charAt(i));
+        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int num : indices) {
+            list.add(num);
         }
+        for (int i=0; i<my_string.length(); i++) {
+            if (!list.contains(i))
+                sb.append(my_string.charAt(i));
+        }
+        answer = sb.toString();
         return answer;
     }
 }
