e6a36f28f251b3d3b818666492d0cdd6c32d4b73
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-11 22:10:43 +0900

    [level 0] Title: 2의 영역, Time: 1.80 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,30 +1,26 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        int min = arr.length;
-        int max = -1;
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
         for (int i=0; i<arr.length; i++) {
-            if (arr[i]==2) {
-                if (i < min) {
-                    min = i;
-                } else if (i > max) {
-                    max = i;
+            if (arr[i] == 2) {
+                if (start < 0) {
+                    start = i;
+                    end = i;
                 }
+                else 
+                    end = i;
             }
         }
-        if (min == arr.length) {
-            list.add(max);
-        } else if (max == -1) {
-            list.add(arr[min]);
-        } else {
-            for (int i=min; i<=max; i++) {
-                list.add(arr[i]);
+        if (start < 0 || end < 0)
+            return new int[]{-1};
+        else {
+            answer = new int[end-start+1];
+            int index = 0;
+            for (int i=start; i<=end; i++) {
+                answer[index++] = arr[i];
             }
-        }
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
         }
         return answer;
     }
