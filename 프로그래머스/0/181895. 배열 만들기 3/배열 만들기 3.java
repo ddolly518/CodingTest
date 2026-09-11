ba6d117748800cf2fd349e92f53eb2fcd6cfaf9a
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-11 22:06:34 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.23 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,15 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
+        int[] answer = {};
         List<Integer> list = new ArrayList<>();
-        for (int[] query : intervals) {
-            int a = query[0];
-            int b = query[1];
-            for (int i=a; i<=b; i++) {
-                list.add(arr[i]);
+        for (int i=0; i<intervals.length; i++) {
+            int[] interval = intervals[i];
+            for (int j=interval[0]; j<=interval[1]; j++) {
+                list.add(arr[j]);
             }
         }
-        int[] answer = new int[list.size()];
+        answer = new int[list.size()];
         for (int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
