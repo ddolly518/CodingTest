52073487f68eda26ce0f65327205b2d75eb02b37
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-13 01:40:45 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,14 +1,18 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
+        
         for (int i=0; i<str_list.length; i++) {
-            if (str_list[i].equals("l")) {
+            String str = str_list[i];
+            
+            if (str.equals("l")) {
                 answer = Arrays.copyOfRange(str_list, 0, i);
-                break;
-            } else if (str_list[i].equals("r")) {
+                return answer;
+            } else if (str.equals("r")) {
                 answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
-                break;
+                return answer;
             }
         }
         return answer;
