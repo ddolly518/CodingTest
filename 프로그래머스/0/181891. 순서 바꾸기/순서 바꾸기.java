ee8be59eef82374e1dfa9d7d79133f78a3b01f63
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-13 01:03:32 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,16 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        for (int i=n; i<num_list.length; i++) {
-            list.add(num_list[i]);
-        }
-        for (int i=0; i<n; i++) {
-            list.add(num_list[i]);
-        }
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
+        int[] answer = new int[num_list.length];
+        int[] one = Arrays.copyOfRange(num_list, n, num_list.length);
+        int[] two = Arrays.copyOfRange(num_list, 0, n);
+        for (int i=0; i<num_list.length; i++) {
+            if (i < one.length) {
+                answer[i] = one[i];
+            } else {
+                answer[i] = two[i - one.length];
+            }
         }
         return answer;
     }
