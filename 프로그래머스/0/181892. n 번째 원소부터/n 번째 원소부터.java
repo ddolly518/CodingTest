8e0c57633773aa3c685d0419e683f2c8edfafb23
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-13 00:16:34 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,9 +1,10 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = {};
-        for (int i=0; i<num_list.length; i++) {
-            answer = Arrays.copyOfRange(num_list, n-1, num_list.length);
+        answer = new int[num_list.length-n+1];
+        int index = 0;
+        for (int i=n-1; i<num_list.length; i++) {
+            answer[index++] = num_list[i];
         }
         return answer;
     }
