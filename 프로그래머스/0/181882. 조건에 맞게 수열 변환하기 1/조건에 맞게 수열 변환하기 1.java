7274edd3282b92f633a1c065bbfd6ad58c3a8010
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-13 22:22:42 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 10.28 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -2,12 +2,14 @@ class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
         for (int i=0; i<arr.length; i++) {
-            if (arr[i]>=50 && arr[i]%2==0) {
-                arr[i]/=2;
-            } else if (arr[i]<50 && arr[i]%2==1) {
-                arr[i]*=2;
+            int num = arr[i];
+            if (num >= 50 && num%2 == 0) {
+                arr[i] = num/2;
+            } else if (num < 50 && num%2 == 1) {
+                arr[i] = num*2;
             }
         }
-        return arr;
+        answer = arr;
+        return answer;
     }
 }
