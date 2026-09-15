5121889650bc79c30c35415af7926a48cf8405aa
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-15 21:46:39 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.25 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String my_string, String alp) {
-        String answer = "";
-        my_string = my_string.replace(alp, alp.toUpperCase());
-        return my_string;
+        String answer = my_string.replaceAll(alp, alp.toUpperCase());
+        return answer;
     }
 }
