4e4c2ef68d69cece0a9dc220ea2be49152463089
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-15 21:15:53 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        for (int n : num_list) {
-            while (n!=1) {
-                if (n%2==0) {
-                    n/=2;
-                    answer++;
+        for (int num : num_list) {
+            while (num!=1) {
+                if (num%2 == 0) {
+                    num/=2;
                 } else {
-                    n=(n-1)/2;
-                    answer++;
-                }  
+                    num = (num-1)/2;
+                }
+                answer++;
             }
         }
         return answer;
