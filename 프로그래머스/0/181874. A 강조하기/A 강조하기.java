a6af47976ce9b6c9a1d3c1a097ee41cf66627cb2
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-15 21:39:59 +0900

    [level 0] Title: A 강조하기, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,12 +1,14 @@
 class Solution {
     public String solution(String myString) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         for (char ch : myString.toCharArray()) {
             if (ch == 'a' || ch == 'A')
-                answer += 'A';
-            else
-                answer += Character.toLowerCase(ch);
+                sb.append(Character.toUpperCase(ch));
+            else 
+                sb.append(Character.toLowerCase(ch));
         }
+        answer = sb.toString();
         return answer;
     }
 }
