51292c2699346f717697094dd534b62538f70ef5
KMS <80440158+ddolly518@users.noreply.github.com>
2026-09-19 18:45:31 +0900

    [level 0] Title: ad 제거하기, Time: 0.53 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
+        String[] answer = {};
         List<String> list = new ArrayList<>();
         for (String str : strArr) {
             if (!str.contains("ad"))
-                list.add(str);                
+                list.add(str);
         }
-        String[] answer = new String[list.size()];
+        answer = new String[list.size()];
         for (int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
